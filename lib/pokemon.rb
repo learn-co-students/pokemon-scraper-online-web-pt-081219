0e741e302fa84dc9f37bc17745974fafd35db662
Joseph Sesti <sestijoey63@gmail.com>
2019-10-25 22:43:16 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Pokemon
   attr_accessor(:name, :type, :db)
   attr_reader(:id)
@@ -6,22 +8,31 @@ class Pokemon
     id:,
     name: "Pikachu",
     type: "Electric",
+    hp: nil,
     db:"pokemon"
     )
     
     @id = id
     @name = name
     @type = type
+    @hp = hp
     @db = db
   end
   
-  def self.save
+  def self.save(name, type, db)
     sql = <<-SQL
-      INSERT INTO pokemon (name, type, db)
-      VALUES (?,?,?,)
+      INSERT INTO pokemon (name, type)
+      VALUES (?,?)
     SQL
     
-    DB[:conn].execute(sql, self.name, self.type, self.db)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0][0]
+    db.execute(sql, name, type)
+    # binding.pry
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0][0]
+  end
+  
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = ? LIMIT 1"
+    result = db.execute(sql, id)[0]
+    Pokemon.new(result[0], result[1], result[2], result[3])
   end
 end
